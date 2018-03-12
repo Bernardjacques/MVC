01c5f70916ef120891b1e1c0578d9850be892a68
Bernardjacques <bernardjacques789@gmail.com>
2018-03-12 16:56:31 +0100

    ok

---
+++ Accueil.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link id="pagestyle" href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/grundschrift" type="text/css"/> 
+    <title>Antiquité, Décoration, Belgique - Meubles Industriels</title>
+</head>
+<body>
+    <img class="bgrd" src="images/bgrd.jpg">
+    <section>
+        <div>
+            <navebar>
+            </navebar>
+        </div>
+        <div class="card">
+            <img src="images/logo.png">
+            Antiquité • Décoration • Aménagements d'intérieur
+            <a href="tel:+32495205934" title="Cliquez pour lancer un appel téléphonique: +32 495/20.59.34">☎  +32 495/20.59.34</a>
+        </div>
+        <div class="menu-button">
+            <input type="button" name="presentation" value="Présentation">
+            <input type="button" name="stock" value="Le Stock">
+            <input type="button" name="contact" value="Contact">
+        </div>
+    </section>
+    <section>   
+        <h2>Le Stock</h2>
+        <p>Fouillez le stock</p>
+        <form method="POST" action="accueil.php">
+        <input type="text" name="search" placeholder="Exemple : Table">
+        <input type="submit" name="submit" value="rechercher">
+        </form>
+        <div>            
+            <h2>Derniers Objets</h2>
+            <img src="images/article01.png">
+            <img src="images/article01.png">
+            <img src="images/article01.png">
+            <img src="images/article01.png">
+            <img src="images/article01.png">
+            <img src="images/article01.png">
+        </div>
+        <div>
+            <input type="button" name="1" value="1">
+            <input type="button" name="2" value="2">
+            <input type="button" name="3" value="3">
+            <input type="button" name="4" value="4">
+            <input type="button" name="5" value="5">
+            <input type="button" name="next" value="Next">
+    </section>
+    <section>
+        <div>
+            <p>Fouillez le stock</p>
+            <form method="POST" action="accueil.php">
+            <input type="text" name="search" placeholder="Exemple : Table">
+            <input type="submit" name="submit" value="rechercher">
+            </form>
+        </div>
+        <div>
+            <p>Liste de mots clés</p>
+        </div>
+    </section>
+    <section>
+        <div>
+            <citation>"L'ennui naquit un jour de l'uniformité"</citation> </br>
+            <author>Antoine Houdar de la Motte</author>
+        </div>
+    </section>
+</body>
+<footer>
+    <p>
+        © 2018 Didier Motte +32 495 20 59 34 - <a class="email" href="mailto:dmotte@skynet.be">dmotte@skynet.be</a> - <a href="https://didiermotte.be/contact/">Plan d'accès</a> </br>
+        Décorateur, Antiquités, Aménagement d'intérieurs, Belgique </br>
+        Restez à jour via: <a href="https://www.facebook.com/antiquites.decoration">Facebook</a>  <a href="https://www.instagram.com/DidierMotte/">Instagram</a> </br>
+        Conçu & développé par <a href="https://pixeline.be">pixeline</a>
+    </p>
+</footer>
+</html>
