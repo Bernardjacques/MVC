58e89e8577bfd62f8309f344628005b70af9710d
Bernardjacques <bernardjacques789@gmail.com>
2018-03-19 09:14:44 +0100

    Pushypusha

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
-<?php
-
-try
-{
-    $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'user');
-}
-catch(Exception $e)
-{
-    die('Erreur : '.$e->getMessage());
-}
-// $req = $db->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
-
-require('modele.php');
-
-$req = getBillets();
-
-require('Accueil.php');
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Didier Motte</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/header.css"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/header1.css"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/presentation.css"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/contact.css"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/footer.css"/>
+    <script src="main.js"></script>
+</head>
+<body>
+    <?php
+        $page = isset($_GET['page'])?htmlentities($_GET['page']):'home';
+        require_once('controller/controller.php');
+    ?>
+</body>
+</html>
